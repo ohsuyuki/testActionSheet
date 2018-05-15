ce583a92ef1bfcf60837fbbd2b971d936e8071b0
ohsuyuki <[ohsu@liquidinc.asia]>
2018-05-15 21:20:40 +0900

    implement action sheet

--- testActionSheet/ViewController.swift
+++ testActionSheet/ViewController.swift
@@ -20,6 +20,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    var floor: Int = 0
+    @IBAction func tapButton(_ sender: Any) {
+        let actionController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
 
+        let action = UIAlertAction(title: "21F", style: .default) { action in self.floor = 21 }
+        actionController.addAction(action)
+
+        for i in stride(from: 7, through: 1, by: -1) {
+            let action = UIAlertAction(title: "\(i)F", style: .default) { action in self.floor = i }
+            actionController.addAction(action)
+        }
+
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel) { action in /* nothing to do */ }
+        actionController.addAction(cancelAction)
+        
+        present(actionController, animated: true, completion: nil)
+    }
+    
 }
 
